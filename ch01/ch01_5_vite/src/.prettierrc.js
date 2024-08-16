8a0ff20055c7c2dbe3f1bfde096d0edf2ba870fc
별찌루 <38304918+JISUSAMA@users.noreply.github.com>
2024-08-16 16:43:01 +0900

    20240816

--- ch01/ch01_5_vite/src/.prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singinQuote: true,
-  tailingComma: 'none',
-  arrowParens:'avoid',
-  semi: false,
-  printWidth: 90
-};
--- ch01/ch01_5_vite/src/App.tsx
+++ ch01/ch01_5_vite/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import {useState} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  //const [count, setCount] = useState(0);
-  console.log("App called");
-  return <h1> Hello React - vite</h1>; 
+  //const [count, setCount] = useState(0)
+  console.log('App called')
+  return <h1>Hello React Vite!</h1>
 }
-export default App;
+export default App
