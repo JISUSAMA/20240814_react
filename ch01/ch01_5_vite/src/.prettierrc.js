be4a5884dd33783a61ff1c7cd7d22ee128046d0a
별찌루 <38304918+JISUSAMA@users.noreply.github.com>
2024-08-16 15:32:51 +0900

    20240816

---
+++ ch01/ch01_5_vite/src/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singinQuote: true,
+  tailingComma: 'none',
+  arrowParens:'avoid',
+  semi: false,
+  printWidth: 90
+};
---
+++ ch01/ch01_5_vite/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  //const [count, setCount] = useState(0);
+  console.log("App called");
+  return <h1> Hello React - vite</h1>; 
+}
+export default App;
