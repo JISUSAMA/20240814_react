74fbd56fac992e99559b3417ba536ddcf769b428
별찌루 <38304918+JISUSAMA@users.noreply.github.com>
2024-08-21 15:07:25 +0900

    20240821

--- ch03/ch03_1/src/pages/Icon.tsx
+++ ch03/ch03_1/src/pages/Icon.tsx
@@ -1,7 +1,7 @@
 export default function Icon() {
   return (
     <div>
-      <div>Icon</div>
+      <h3>Icon</h3>
       <span className="material-symbols-outlined">home</span>
       <span className="material-symbols-outlined">check_circle_outline</span>
     </div>
--- ch03/ch03_1/src/pages/UsingIconWithCssClass.tsx
+++ ch03/ch03_1/src/pages/UsingIconWithCssClass.tsx
@@ -1,3 +1,13 @@
+import {Icon3} from '../components'
 export default function UsingIconWithCssClass() {
-  return <div>UsingIconWithCssClass</div>
+  return (
+    <div>
+      <h3>UsingIconWithCssClass</h3>
+      <Icon3
+        name="check_circle_outline"
+        className="text-red"
+        style={{fontSize: '50px'}}
+      />
+    </div>
+  )
 }
