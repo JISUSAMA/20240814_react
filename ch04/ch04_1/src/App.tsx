0490eb842eed9a0fa3fe96ae70cc1e9e24262ec9
별찌루 <38304918+JISUSAMA@users.noreply.github.com>
2024-08-26 16:16:15 +0900

    20240826

--- ch04/ch04_1/src/App.tsx
+++ ch04/ch04_1/src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useRef, useEffect, useState} from 'react'
+import Clock from './pages/Clock'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  let today = new Date()
+  // setInterval 함수는 페이지가 호출될 때 한번만 호출되어야 한다.
+  const id = setInterval(() => {
+    today = new Date()
+    console.log(today)
+    const clock = document.querySelector('div>Clock')
+    //시간 업데이트를 위한 구문이 필요
+  }, 1000)
+  // useEffect 사용 이유는 컴포넌트가 생성시 한번만 호출되어야 하기 때문
+  useEffect(() => {
+    id
+    return function () {
+      clearInterval(id)
+    }
+  }, []) //[]는 의존성목록이고 목록이 비었으므로 컴포넌트생성시에만 호출
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="w-full h-full">
+      <Clock today={today}></Clock>
+    </div>
   )
 }
 
 export default App
+
+/* 일반적인 자바스크립트로 시간 갱신 할 때
+<!DOCTYPE html>
+<html lang="ko">
+<head></head>
+<body><h1 id="time">요소의 중앙 배치 flex 이용</h1></body>
+<script>
+  let today = new Date()
+  var time = document.getElementById("time")
+  setInterval(function () {
+    time.textContent = new Date()
+  }, 1000)
+</script>
+</html>
+*/
---
+++ ch04/ch04_1/src/pages/Clock.tsx
@@ -0,0 +1,15 @@
+import {FC} from 'react'
+import {Div, Title, Subtitle} from '../components'
+
+export type ClockProps = {today: Date}
+
+const Clock: FC<ClockProps> = ({today}) => {
+  return (
+    <Div className="flex flex-col items-center justify-center h-32 w-screen bg-primary text-white">
+      <Title className="text-5xl">{today.toLocaleTimeString()}</Title>
+      <Subtitle className="mt-4 text-2xl">{today.toLocaleDateString()}</Subtitle>
+    </Div>
+  )
+}
+
+export default Clock
