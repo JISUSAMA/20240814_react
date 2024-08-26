f92261e60c0b86a720565ed86a4ab4a3ba643ee0
별찌루 <38304918+JISUSAMA@users.noreply.github.com>
2024-08-26 16:18:15 +0900

    Update App.tsx

--- ch04/ch04_1/src/App.tsx
+++ ch04/ch04_1/src/App.tsx
@@ -3,21 +3,44 @@ import Clock from './pages/Clock'
 
 function App() {
   const [count, setCount] = useState(0)
-  let today = new Date()
-  // setInterval 함수는 페이지가 호출될 때 한번만 호출되어야 한다.
-  const id = setInterval(() => {
-    today = new Date()
-    console.log(today)
-    const clock = document.querySelector('div>Clock')
-    //시간 업데이트를 위한 구문이 필요
-  }, 1000)
-  // useEffect 사용 이유는 컴포넌트가 생성시 한번만 호출되어야 하기 때문
+  // let today = new Date()
+
+  //  1) setInterval 함수는 페이지가 호출될때 한번만 호출되어야 한다.
+  // /*const id = setInterval(() => {
+  //   today = new Date()
+  //   console.log(today)
+  //   document.querySelector('div>Clock')
+  //   //시간 업데이트를 위한 구문이 필요
+  // }, 1000)*/
+
+  // 2) useEffect를 사용하는 이유는 컴포넌트가 생성시 한번만 호출되어야 하기 때문
+  // useEffect(() => {
+  //   id
+  //   return function () {
+  //     clearInterval(id)
+  //   }
+  // }, []) //[] 는 의존성 목록이고, 목록이 비었으므로 컴포넌트생성시에만 호출
+
+  //3) 메서드 호출에 관여하는 useRef 훅 사용
+  // let today = useRef(new Date())
+  // useEffect(() => {
+  //   const id = setInterval(() => {
+  //     today.current = new Date()
+  //     console.log(today.current.toLocaleTimeString())
+  //   }, 1000)
+  //   return () => clearInterval(id)
+  // })
+
+  //4) 데이터 관리(useMemo, useCallback, useState, useReducer) 중
+  // useState 훅 사용
+  const [today, setToday] = useState(new Date())
   useEffect(() => {
-    id
-    return function () {
-      clearInterval(id)
-    }
-  }, []) //[]는 의존성목록이고 목록이 비었으므로 컴포넌트생성시에만 호출
+    const id = setInterval(() => {
+      setToday(new Date())
+    }, 1000)
+    return () => clearInterval(id)
+  }, [])
+
   return (
     <div className="w-full h-full">
       <Clock today={today}></Clock>
@@ -26,18 +49,3 @@ function App() {
 }
 
 export default App
-
-/* 일반적인 자바스크립트로 시간 갱신 할 때
-<!DOCTYPE html>
-<html lang="ko">
-<head></head>
-<body><h1 id="time">요소의 중앙 배치 flex 이용</h1></body>
-<script>
-  let today = new Date()
-  var time = document.getElementById("time")
-  setInterval(function () {
-    time.textContent = new Date()
-  }, 1000)
-</script>
-</html>
-*/
