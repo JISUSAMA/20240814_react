ddf897642098a626f24835cdff83ebf7dc4371c1
별찌루 <38304918+JISUSAMA@users.noreply.github.com>
2024-09-02 12:20:57 +0900

    20240902

--- ch05/ch05_3/src/store/useStore.ts
+++ ch05/ch05_3/src/store/useStore.ts
@@ -2,16 +2,21 @@ import {configureStore} from '@reduxjs/toolkit'
 import {useMemo} from 'react'
 import {rootReducer} from './rootReducer'
 // import logger from './logger'
-//import logger from 'redux-logger'
+import logger from 'redux-logger'
 // thunk :: action을 함수로서 호출해 주는 기능을 추가한 미들웨어
 import thunk from 'redux-thunk'
 
-//const useLogger = process.env.NODE_ENV !== 'production'
+const useLogger = process.env.NODE_ENV !== 'production'
 
 const initializeStore = () => {
   const middleware: any[] = [thunk]
-  //if (useLogger) middleware.push(logger)
-  const store = configureStore({reducer: rootReducer, middleware})
+  if (useLogger) middleware.push(logger)
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware => {
+      return []
+    }
+  })
   return store
 }
 
